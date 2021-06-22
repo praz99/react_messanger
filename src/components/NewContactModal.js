87732cbde61650762925d974db16ac87d5ed9391
Praz <t.prazwal@gmail.com>
2021-06-22 22:18:57 +0200

    Add the Modal header and body for NewContactModal

--- src/components/NewContactModal.js
+++ src/components/NewContactModal.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Modal, Form, Button } from 'react-bootstrap';
 
-function NewContactModal() {
+function NewContactModal({ closeModal }) {
+  const idRef = useRef();
+  const nameRef = useRef();
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // createContact(idRef.current.value, nameRef.current.value)
+    closeModal();
+  }
   return (
-    <div>New Contact Modal</div>
+    <>
+      <Modal.Header closeButton>Create Contact</Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Label>Id</Form.Label>
+            <Form.Control type="text" ref={idRef} required />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" ref={nameRef} required />
+          </Form.Group>
+          <Button type="submit">Create</Button>
+        </Form>
+      </Modal.Body>
+    </>
   );
 }
 
