0704eb08ec1f7b4223f8691f317c9d1f46bea537
Praz <t.prazwal@gmail.com>
2021-06-22 22:40:01 +0200

    add ContactsProvider context

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import UseLocalStorage from "../hooks/useLocalStorage";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
+import { ContactsProvider } from "../contexts/ContactsProvider";
 
 function App() {
   const [id, setId] = UseLocalStorage('id')
+
+  const dashboard = (
+    <ContactsProvider>
+      <Dashboard id={id} />
+    </ContactsProvider>
+  )
   return (
-    id ? <Dashboard id={id} /> : <Login onIdSubmit={setId} />
+    id ? dashboard : <Login onIdSubmit={setId} />
   );
 }
 
--- src/components/NewContactModal.js
+++ src/components/NewContactModal.js
@@ -1,13 +1,16 @@
 import React, { useRef } from 'react';
 import { Modal, Form, Button } from 'react-bootstrap';
+import { useContacts } from '../contexts/ContactsProvider';
 
 function NewContactModal({ closeModal }) {
   const idRef = useRef();
   const nameRef = useRef();
+  const { createContact } = useContacts();
+
   function handleSubmit(e) {
     e.preventDefault();
 
-    // createContact(idRef.current.value, nameRef.current.value)
+    createContact(idRef.current.value, nameRef.current.value)
     closeModal();
   }
   return (
