07ecde2fa02523521fda534480a962aab0af59db
Praz <t.prazwal@gmail.com>
2021-06-23 17:40:05 +0200

    display messages in OpenConversation

--- src/components/OpenConversation.js
+++ src/components/OpenConversation.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Form, InputGroup, Button } from 'react-bootstrap';
 import { useConversations } from '../contexts/ConversationsProvider';
 
 function OpenConversation() {
   const [text, setText] = useState('');
+  const lastMessageRef = useRef();
   const { sendMessage, selectedConversation } = useConversations();
 
   function handleSubmit(e) {
@@ -12,9 +13,36 @@ function OpenConversation() {
     setText('');
   }
 
+  useEffect(() => {
+    if (lastMessageRef.current) {
+      lastMessageRef.current.scrollIntoView({ smooth: true });
+    }
+  })
+
   return (
     <div className="d-flex flex-column flex-grow-1">
-      <div className="flex-grow-1 overflow-auto"></div>
+      <div className="flex-grow-1 overflow-auto">
+        <div className="d-flex flex-column align-items-start justify-content-end px-3">
+          {selectedConversation.messages.map((message, index) => {
+            const lastMessage = selectedConversation.messages.length - 1 === index;
+            return (
+              <div
+                ref={lastMessage ? lastMessageRef : null}
+                key={index}
+                className={`my-1 d-flex flex-column ${message.fromMe ? 'align-self-end' : ''}`}
+              >
+                <div
+                  className={`rounded px-2 py-1 ${message.fromMe ? 'bg-primary text-white' : 'border'}`}>
+                  {message.text}
+                </div>
+                <div className={`text-muted small ${message.fromMe ? 'text-right' : ''}`}>
+                  {message.fromMe ? 'You' : message.senderName}
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
       <Form onSubmit={handleSubmit}>
         <Form.Group className="m-2">
           <InputGroup>
--- src/contexts/ConversationsProvider.js
+++ src/contexts/ConversationsProvider.js
@@ -53,13 +53,23 @@ export function ConversationsProvider({ id, children }) {
     const recipients = conversation.recipients.map(recipient => {
       const contact = contacts.find(contact => {
         return contact.id === recipient;
-      })
-      const name = (contact && contact.name) || recipient
+      });
+      const name = (contact && contact.name) || recipient;
       return { id: recipient, name }
-    })
-    const selected = index === selectedConversationIndex
+    });
 
-    return { ...conversation, recipients, selected }
+    const messages = conversation.messages.map(message => {
+      const contact = contacts.find(contact => {
+        return contact.id === message.sender;
+      });
+      const name = (contact && contact.name) || message.sender;
+      const fromMe = id === message.sender;
+      return { ...message, senderName: name, fromMe };
+    });
+
+    const selected = index === selectedConversationIndex;
+
+    return { ...conversation, messages, recipients, selected };
   })
 
   const value = {
