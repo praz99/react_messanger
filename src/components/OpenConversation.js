b112b5da61480948139f7bfb713613f353ab3880
Praz <t.prazwal@gmail.com>
2021-06-23 16:47:39 +0200

    add textarea and send button inside OpenConversation

--- src/components/OpenConversation.js
+++ src/components/OpenConversation.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form, InputGroup, Button } from 'react-bootstrap';
+import { useConversations } from '../contexts/ConversationsProvider';
 
 function OpenConversation() {
+  const [text, setText] = useState('');
+  const { sendMessage, selectedConversation } = useConversations();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    sendMessage(selectedConversation.recipients.map(recipient => recipient.id), text);
+    setText('');
+  }
+
   return (
-    <div></div>
+    <div className="d-flex flex-column flex-grow-1">
+      <div className="flex-grow-1 overflow-auto"></div>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="m-2">
+          <InputGroup>
+            <Form.Control
+              as="textarea"
+              required
+              value={text}
+              onChange={e => setText(e.target.value)}
+              style={{ height: '75px', resize: 'none' }}
+            />
+            <InputGroup.Append>
+              <Button type="submit">Send</Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </Form.Group>
+      </Form>
+    </div>
   )
 }
 
